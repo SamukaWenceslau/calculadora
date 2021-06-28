8c9fd9b1f2739dd344712bff325bd0274728636f
SamukaWenceslau <samukawens@gmail.com>
2021-06-28 01:06:37 -0300

    fazendo testes falharem (multiplicação e divisão)

--- __tests__/Calculadora.test.js
+++ __tests__/Calculadora.test.js
@@ -13,7 +13,14 @@ describe("Calculos básicos", () => {
     expect(resultado).toEqual(-15);
   })
 
-  
+  it("Deve retornar o resultado da multiplicação de N valores", () => {
+    let resultado = Calculadora.multiplicar(2, 2, 2);
+    expect(resultado).toEqual(8);
+  })
 
+  it("Deve retornar o resultado da divisão de N valores", () => {
+    let resultado = Calculadora.dividir(20, 2, 2);
+    expect(resultado).toEqual(5);
+  })
 
 })
