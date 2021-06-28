e3a070c9a9ad95f8c6464f9b60304227debfc58b
SamukaWenceslau <samukawens@gmail.com>
2021-06-28 00:32:50 -0300

    fazendo o teste falhar

---
+++ __tests__/Calculadora.test.js
@@ -0,0 +1,12 @@
+import Calculadora from "../src/Calculadora";
+
+
+describe("Calculos básicos", () => {
+
+  it("Deve retornar o resultado da soma de 2 valores", () => {
+    let resultado = Calculadora.somar(15, 30);
+    expect(resultado).toEqual(45);
+  })
+
+
+})
