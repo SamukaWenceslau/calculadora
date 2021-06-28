71a3e2998afec97746fae1bccfb5fb0272902e89
SamukaWenceslau <samukawens@gmail.com>
2021-06-28 00:46:11 -0300

    fazendo o teste falhar (subtração)

--- __tests__/Calculadora.test.js
+++ __tests__/Calculadora.test.js
@@ -8,5 +8,12 @@ describe("Calculos básicos", () => {
     expect(resultado).toEqual(45);
   })
 
+  it("Deve retornar o resultado da subtração de 2 valores", () => {
+    let resultado = Calculadora.subtrair(15, 30);
+    expect(resultado).toEqual(15);
+  })
+
+  
+
 
 })
